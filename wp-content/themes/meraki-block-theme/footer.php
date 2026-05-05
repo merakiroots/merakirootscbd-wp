3df19086c6d750475ca0104c8816512c6f236858
merakiroots <info@merakirootscbd.com>
2026-05-05 01:46:48 -0700

    Add Meraki block theme scaffold

---
+++ wp-content/themes/meraki-block-theme/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Classic footer wrapper for WooCommerce PHP templates.
+ *
+ * @package MerakiBlockTheme
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+</main>
+<?php block_template_part( 'footer' ); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
